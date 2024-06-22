65270be2f6dc8c8461131b606345047f5a226148
Joaquín Varela <joaqo9909@gmail.com>
2024-06-22 11:06:08 -0300

    Custom dataset class

---
+++ custom_dataset.py
@@ -0,0 +1,13 @@
+from torch.utils.data import Dataset
+
+class MusicalInstrumentsDataset(Dataset):
+
+    def __init__(self, annotations_file, audio_dir):
+
+    def __len__(self):
+        """Returns number of samples in dataset"""
+        pass
+
+    def __getitem__(self, index):
+        """Returns item form x index"""
+        pass
